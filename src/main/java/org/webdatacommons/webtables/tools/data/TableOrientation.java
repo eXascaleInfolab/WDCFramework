710f30d5e4f4633657ec74f935db3488e1b5fece
rmeusel <rmeusel@7b60a537-7e44-4597-9335-3e5a59bbdbef>
2015-11-13 14:22:40 +0000

    moved dwt-tool package into extraction framework
    git-svn-id: https://subversion.assembla.com/svn/commondata/WDCFramework/trunk@261 7b60a537-7e44-4597-9335-3e5a59bbdbef

---
+++ src/main/java/org/webdatacommons/webtables/tools/data/TableOrientation.java
@@ -0,0 +1,11 @@
+package org.webdatacommons.webtables.tools.data;
+
+import java.io.Serializable;
+
+public enum TableOrientation implements Serializable{
+	HORIZONTAL,
+	VERTICAL,
+	MIXED,
+	NOT_IDENTIFIABLE
+
+}
---
+++ src/main/java/org/webdatacommons/webtables/tools/data/TableType.java
@@ -0,0 +1,11 @@
+package org.webdatacommons.webtables.tools.data;
+
+import java.io.Serializable;
+
+public enum TableType implements Serializable {
+	LAYOUT,
+	RELATION,
+	MATRIX,
+	ENTITY,
+	OTHER
+}
